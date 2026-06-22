6ed2cc53916d85d8d192b1fd10bba7079976f684
SaiGokul654 <saigokul850@gmail.com>
2026-06-22 15:09:00 +0530

    Initail Commit

---
+++ pro5/src/main/java/com/batch3/pro5/App.java
@@ -0,0 +1,13 @@
+package com.batch3.pro5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
